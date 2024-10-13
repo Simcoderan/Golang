e66621f940d1c446b65ac673e809fb0e6710dc25
simran <simransingh956429@gmail.com>
2024-10-13 16:52:21 +0530

    hi

--- 2_simple_values/main.go
+++ 2_simple_values/main.go
@@ -4,9 +4,15 @@ import "fmt"
 func main(){
 	//simple value
 	fmt.Println(1)      //integer
-	fmt.Println(1+1)
+	fmt.Println(1+1)    //addition
 
 	//boolean
 	fmt.Println(true)
 	fmt.Println(false)
+
+	//floats
+	fmt.Println(10.5)
+
+	//arithmatics
+	fmt.Println(7.0/3.0)
 }
