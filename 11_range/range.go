1e9b78bbce61a9e1dcb6ee8389be63009e74c0c5
simran <simransingh956429@gmail.com>
2024-12-05 08:51:34 +0530

    range

--- 11_range/range.go
+++ 11_range/range.go
@@ -13,11 +13,27 @@ func main(){
 	//for i:=0;i<len(nums);i++{
 		//fmt.Println(nums[i])
 	//}
-     sum:=0
-	for_,num:=range nums{
-		sum=sum+num
+     //sum:=0
+	for i ,num:=range nums{    //i=index
+		//sum=sum+num
 		//fmt.Println(num)
+		fmt.Println(num,i)
 	}
-	fmt.Println(sum)
+	//fmt.Println(sum)
 
+	m:=map[string]string{"fname":"john","lname":"doe"}
+
+	for k,v:=range m{
+		fmt.Println(k,v)
+	}
+
+	//range for string
+	for i,c:=range "golang"{
+		fmt.Println(i,c)   //unicode code point "rune"(data structure)  for alpjhabets -output
+	}                       //starting byte of rune
+	                        //255-.1 byte ,2 bytes if more than 255(unicode has more than 255 as of assci value)
+
+							
+	//fmt.Println(i,string(c))  //for character printing						
+ 
 }
